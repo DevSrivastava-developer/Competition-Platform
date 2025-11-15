b5524521f977790ca40c1206c4b69212066e67ac
Dev Srivastava <devsrivastav10094@gmail.com>
2025-11-15 20:07:27 +0000

    changes

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: [
+    'http://localhost:3000',
+    'https://competition-platform-frontend.vercel.app' // <-- ADD THIS
+  ],
     credentials: true,
   });
 
